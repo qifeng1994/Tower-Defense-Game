e5e9ce13b74dac9e45a3a11569fa20c57fe22f20
qifeng1994 <maidoufengfeng@gmail.com>
2021-07-18 23:58:40 +0900

    add bullet and hit effect

---
+++ TowerDefenseGame/Assets/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+    public GameObject impactEffect;
+
+    public float speed = 70f;
+
+    public void Seek(Transform _target) //希望把一个来自其他类的transform赋值给这里的target
+    {
+        target = _target;
+    }
+
+    private void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame) //当向量的长度接近要移动的距离时，说明子弹打到了敌人
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World); //位移
+    }
+
+    void HitTarget()
+    {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation); //
+        Destroy(effectIns, 2f);
+
+        Destroy(target.gameObject);
+
+        Destroy(gameObject);
+    }
+
+}
--- TowerDefenseGame/Assets/Turret.cs
+++ TowerDefenseGame/Assets/Turret.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+
+    [Header("Attributes")]
     public float range = 15f; //炮台的锁敌范围
+    public float fireRate = 1f; //每秒发射一次
+    private float fireCountdown = 0f; //每次射击间隔的时间
+
+    [Header("Unity Setup Fields")]
     public float turnSpeed = 10; //炮台转向的速度
-
     public string enemyTag = "Enemy";
-
     public Transform partToRotate; //炮台需要转动的是上半部分
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     private void Start()
     {
@@ -53,6 +59,23 @@ public class Turret : MonoBehaviour
         // Vector3 rotation = lookRatation.eulerAngles; //以欧拉角来表示旋转
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRatation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f); //绕y轴旋转
+
+        if(fireCountdown<=0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); //子弹实例化
+        Bullet bullet = bulletGo.GetComponent<Bullet>(); //通过这样的方式可以确定子弹的位置信息
+
+        if (bullet != null)
+            bullet.Seek(target); //把enemy的信息转递给bullet类
+
     }
 
 
